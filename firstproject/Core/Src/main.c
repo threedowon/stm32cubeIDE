de061fe4dd3f59ded02027a4ed126245f4a94032
threedowon <3dowon@gmail.com>
2026-02-05 03:06:12 +0900

    ~6강 1/2

--- firstproject/Core/Src/main.c
+++ firstproject/Core/Src/main.c
@@ -166,12 +166,12 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : GPIO_LED_Pin */
   GPIO_InitStruct.Pin = GPIO_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIO_LED_GPIO_Port, &GPIO_InitStruct);
 
--- study/Core/Inc/main.h
+++ study/Core/Inc/main.h
@@ -57,10 +57,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define GPIO_LED_Pin GPIO_PIN_13
-#define GPIO_LED_GPIO_Port GPIOC
-#define GPIO_SWITCH_Pin GPIO_PIN_0
-#define GPIO_SWITCH_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
